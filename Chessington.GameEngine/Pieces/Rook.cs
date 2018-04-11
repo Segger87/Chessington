c43bc4a4f276ff52c7d8a85d8d785847c42a532c
Segger87 <Segger87@gmail.com>
2018-04-11 15:15:25 +0100

    Added lateral movement for Rook

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -10,7 +11,24 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var myLocation = board.FindPiece(this);
+            var legalMoves = new List<Square>();
+
+            for (var i = 0; i < 8; i++)
+            {
+                var placesRookCanMoveTo = new Square(i, myLocation.Col);
+                if (placesRookCanMoveTo != myLocation)
+                {
+                    legalMoves.Add(placesRookCanMoveTo);
+                }
+                placesRookCanMoveTo = new Square(myLocation.Row, i);
+                if (placesRookCanMoveTo != myLocation)
+                {
+                    legalMoves.Add(placesRookCanMoveTo);
+                }
+            }
+
+            return legalMoves;
         }
     }
 }
