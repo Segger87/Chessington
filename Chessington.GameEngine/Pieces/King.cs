cdf3330cd06377c22cfcde7688604a276ad49d2d
Segger87 <Segger87@gmail.com>
2018-04-12 10:07:38 +0100

    Added King Movement

--- Chessington.GameEngine/Pieces/King.cs
+++ Chessington.GameEngine/Pieces/King.cs
@@ -10,7 +10,30 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var myLocation = board.FindPiece(this);
+            var legalMoves = new List<Square>();
+            var movesKingCanMakeForwardsCol = new Square(myLocation.Row, myLocation.Col + 1);
+            var movesKingCanMakeForwardsRow = new Square(myLocation.Row, myLocation.Col - 1);
+            var movesKingCanMakeBackwardsCol = new Square(myLocation.Row + 1, myLocation.Col);
+            var movesKingCanMakeBackwardsRow = new Square(myLocation.Row - 1, myLocation.Col);
+            var movesKingCanMakeDiagForward = new Square(myLocation.Row + 1, myLocation.Col + 1);
+            var movesKingCanMakeDiagUp= new Square(myLocation.Row + 1, myLocation.Col - 1);
+            var movesKingCanMakeDiagBackward2 = new Square(myLocation.Row - 1, myLocation.Col + 1);
+            var movesKingCanMakeDiagBackward = new Square(myLocation.Row - 1, myLocation.Col - 1);
+
+            if (movesKingCanMakeForwardsCol != myLocation)
+            {
+                legalMoves.Add(movesKingCanMakeBackwardsCol);
+                legalMoves.Add(movesKingCanMakeForwardsRow);
+                legalMoves.Add(movesKingCanMakeBackwardsRow);
+                legalMoves.Add(movesKingCanMakeForwardsCol);
+                legalMoves.Add(movesKingCanMakeDiagForward);
+                legalMoves.Add(movesKingCanMakeDiagBackward);
+                legalMoves.Add(movesKingCanMakeDiagUp);
+                legalMoves.Add(movesKingCanMakeDiagBackward2);
+            }
+
+            return legalMoves;
         }
     }
 }
